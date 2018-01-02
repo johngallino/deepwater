dfecfdf693ced4f97f9fac730cec5ecbe09bc460
johngallino <34987867+johngallino@users.noreply.github.com>
2018-01-02 14:38:24 -0500

    janky crosshair
    things look better
    wall can be knocked over
    crosshair almost works

---
+++ Assets/Scripts/shooting.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shooting : MonoBehaviour {
+
+    public int gunDamage = 1;
+    public float fireRate = .25f;
+    public float weaponRange = 30f;
+    public float hitForce = 100f;
+    public GameObject crosshairPrefab;
+    public Vector3 gunPos;
+    public Ray shootRay;
+
+    private WaitForSeconds shotDuration = new WaitForSeconds(.07f);
+    //private AudioSource gunAudio;
+    private LineRenderer laserLine;
+    private float nextFire;
+    private bool shotFired;
+
+    // Use this for initialization
+    void Start () {
+    shotFired = false;
+    Debug.Log("shotFired : " + shotFired);
+    laserLine = GetComponent<LineRenderer>();
+    //gunAudio = GetComponent<AudioSource>();
+
+    if (crosshairPrefab != null)
+        {
+         crosshairPrefab = Instantiate(crosshairPrefab);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        PositionCrosshair(shootRay);
+        gunPos = transform.position + new Vector3(0, 2, 0);
+
+        if (Input.GetButtonDown("Fire1") && Time.time > nextFire)
+
+        {
+            nextFire = Time.time + fireRate;
+            StartCoroutine(ShotEffect());
+            RaycastHit hit;
+            laserLine.SetPosition(0, gunPos);
+
+            if (Physics.Raycast(gunPos, transform.forward, out hit, weaponRange))
+            {
+                laserLine.SetPosition(1, hit.point);
+                shootableEvilCube health = hit.collider.GetComponent<shootableEvilCube>();
+
+                if (health != null)
+                {
+                    health.Damage(gunDamage);
+                }
+
+                if (hit.rigidbody != null)
+                {
+                    Debug.Log("Ray hit a rigidbody");
+                    hit.rigidbody.AddForce(-hit.normal * hitForce);
+                }
+                else if (hit.collider != null)
+                    Debug.Log("Ray hit a collider");
+                else if (hit.collider == null && hit.rigidbody == null)
+                    Debug.Log("Ray hit nothing");
+            }
+            else
+            {
+                laserLine.SetPosition(1, gunPos + (transform.forward * weaponRange));
+            }
+        }
+        
+        
+    }
+
+    private IEnumerator ShotEffect()
+
+    {
+        //gunAudio.Play();
+        shotFired = true;
+        //Debug.Log("Shot fired");
+        Debug.Log("shotFired : " + shotFired);
+        laserLine.enabled = true;
+        yield return shotDuration;
+        laserLine.enabled = false;
+        shotFired = false;
+
+    }
+
+    void PositionCrosshair(Ray ray)
+    {
+        Vector3 gunPos = transform.position + new Vector3(0, 2, 0);
+
+        RaycastHit hit;
+        if (Physics.Raycast(gunPos, transform.forward, out hit, weaponRange))
+            if (crosshairPrefab != null)
+                {
+            crosshairPrefab.transform.position = hit.point;
+            crosshairPrefab.transform.LookAt(Camera.main.transform);
+
+                }
+    }
+}
---
+++ Assets/Scripts/swimscriptv2.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+    public class swimscriptv2 : MonoBehaviour
+    {
+
+        public float walkSpeed = 5.0f;
+        public float runSpeed = 30.0f;
+        public float mouseSensitivity = 5.0f;
+        public float clampXAngle = 35.0f;
+        public bool lockCursor = true;
+       
+
+
+        private float rotY = 0.0f; // rotation around the up/y axis
+        private float rotX = 0.0f; // rotation around the right/x axis
+        private float movementSpeed;
+        private bool m_cursorIsLocked;
+
+
+    // Use this for initialization
+    void Start()
+        {
+        Vector3 rot = transform.localRotation.eulerAngles;
+        rotY = rot.y;
+        rotX = rot.x;
+
+        
+    }
+
+        // Update is called once per frame
+        void Update()
+        {
+            float horizontal = Input.GetAxis("Strafe"); // slide on x axis
+            float forwardback = Input.GetAxis("ForwardBack"); // slide on z axis
+            float updown = Input.GetAxis("UpDown"); // slide on y axis
+            float movementSpeed = 3.0f;
+
+        
+        UpdateCursorLock();
+
+        movementSpeed = walkSpeed;
+
+        if (Input.GetButton("Sprint"))
+        {
+            movementSpeed = runSpeed;
+        }
+
+        //this is movement
+        transform.Translate(horizontal * movementSpeed * Time.deltaTime, updown * movementSpeed * Time.deltaTime, forwardback * movementSpeed * Time.deltaTime);
+
+            //transform.position += direction * movementSpeed * Time.deltaTime;
+
+            //this is rotation
+            float mouseX = Input.GetAxis("Mouse X");
+            float mouseY = -Input.GetAxis("Mouse Y");
+
+            rotY += mouseX * mouseSensitivity * Time.deltaTime; // left and right
+            rotX += mouseY * mouseSensitivity * Time.deltaTime; // up and down
+
+            rotX = Mathf.Clamp(rotX, -clampXAngle, clampXAngle); // up and down clamp
+
+            Quaternion localRotation = Quaternion.Euler(rotX, rotY, 0.0f);
+            transform.rotation = localRotation;
+        
+    }
+
+    public void UpdateCursorLock()
+    {
+        //if the user set "lockCursor" we check & properly lock the cursos
+        if (lockCursor)
+            InternalLockUpdate();
+    }
+
+    public void SetCursorLock(bool value)
+    {
+        lockCursor = value;
+        if (!lockCursor)
+        {//we force unlock the cursor if the user disable the cursor locking helper
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+    }
+
+    private void InternalLockUpdate()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            m_cursorIsLocked = false;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            m_cursorIsLocked = true;
+        }
+
+        if (m_cursorIsLocked)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+        else if (!m_cursorIsLocked)
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+    }
+
+
+}
+
